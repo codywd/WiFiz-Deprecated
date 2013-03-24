fc220c93ed238827f141041b561482d46a0e7aba
GrayHatter <greg@grayhatter.com>
2013-03-23 21:32:01 -0700

    STYLE changes, max width in now 80, ex licence, etc

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ pid_file = 'program.pid'
 # Lets make sure we're root as well #
 euid = os.geteuid()
 if euid != 0:
-    print "WiFiz needs to be run as root, we're going to sudo for you. You can ctrl-c to exit.\n"
+    print ("WiFiz needs to be run as root, we're going to sudo for you. "
+            "You can ctrl-c to exit.\n")
     args = ['gksudo', sys.executable] + sys.argv + [os.environ]
     os.execlpe('sudo', *args)
 
@@ -41,8 +42,10 @@ except IOError:
 # main class #
 class WiFiz(wx.Frame):
     def __init__(self, parent, title):
-        super(WiFiz, self).__init__(None, title="WiFiz", style = wx.DEFAULT_FRAME_STYLE)
-        self.TrayIcon = tbi.Icon(self, wx.Icon("./imgs/logo.png", wx.BITMAP_TYPE_PNG), "WiFiz")
+        super(WiFiz, self).__init__(None, title="WiFiz", 
+                            style = wx.DEFAULT_FRAME_STYLE)
+        self.TrayIcon = tbi.Icon(self, wx.Icon("./imgs/logo.png", 
+                                    wx.BITMAP_TYPE_PNG), "WiFiz")
         self.index = 0
         self.InitUI()
 
@@ -56,7 +59,8 @@ class WiFiz(wx.Frame):
         self.mainMenu = wx.MenuBar()
 
         fileMenu = wx.Menu()
-        ScanAPs = fileMenu.Append(wx.ID_ANY, "Scan for New Networks", "Scan for New Wireless Networks.")
+        ScanAPs = fileMenu.Append(wx.ID_ANY, "Scan for New Networks", 
+                                    "Scan for New Wireless Networks.")
         fileMenu.AppendSeparator()
         fileQuit = fileMenu.Append(wx.ID_EXIT, "Quit", "Exit the Program.")
         self.mainMenu.Append(fileMenu, "&File")
@@ -70,12 +74,15 @@ class WiFiz(wx.Frame):
         self.mainMenu.Append(profilesMenu, "Profiles")
 
         helpMenu = wx.Menu()
-        helpItem = helpMenu.Append(wx.ID_HELP, "Help with WiFiz", "Get help with WiFiz")
+        helpItem = helpMenu.Append(wx.ID_HELP, "Help with WiFiz", 
+                                            "Get help with WiFiz")
         helpItem.Enable(False)
         helpMenu.AppendSeparator()
-        reportIssue = helpMenu.Append(wx.ID_ANY, "Report an Issue...", "Report a bug, or give a suggestion.")
+        reportIssue = helpMenu.Append(wx.ID_ANY, "Report an Issue...", 
+                                    "Report a bug, or give a suggestion.")
         helpMenu.AppendSeparator()
-        aboutItem = helpMenu.Append(wx.ID_ABOUT, "About WiFiz", "About this program...")
+        aboutItem = helpMenu.Append(wx.ID_ABOUT, "About WiFiz", 
+                                        "About this program...")
         self.mainMenu.Append(helpMenu, "&Help")
 
         self.SetMenuBar(self.mainMenu)
@@ -84,17 +91,29 @@ class WiFiz(wx.Frame):
 
         # Create Popup Menu #
         self.PopupMenu = wx.Menu()
-        popCon = self.PopupMenu.Append(wx.ID_ANY, "Connect to Network", "Connect to selected network.")
-        popDCon = self.PopupMenu.Append(wx.ID_ANY, "Disconnect from Network", "Disconnect from selected network.")
+        popCon = self.PopupMenu.Append(wx.ID_ANY, "Connect to Network", 
+                                            "Connect to selected network.")
+        popDCon = self.PopupMenu.Append(wx.ID_ANY, "Disconnect from Network", 
+                                        "Disconnect from selected network.")
 
         # Create Toolbar #
         toolbar = self.CreateToolBar()
-        newTool = toolbar.AddLabelTool(wx.ID_NEW, 'New', wx.ArtProvider.GetBitmap(wx.ART_NEW), wx.NullBitmap, wx.ITEM_NORMAL, 'New Connection')
-        ReScanAPs = toolbar.AddLabelTool(wx.ID_ANY, 'Scan', wx.Bitmap('imgs/APScan.png'), wx.NullBitmap, wx.ITEM_NORMAL, 'Scan')
-        connectSe = toolbar.AddLabelTool(wx.ID_ANY, 'Connect', wx.Bitmap('imgs/connect.png'), wx.NullBitmap, wx.ITEM_NORMAL, 'Connect')
-        dConnectSe = toolbar.AddLabelTool(wx.ID_ANY, 'Disconnect', wx.Bitmap('imgs/disconnect.png'), wx.NullBitmap, wx.ITEM_NORMAL, 'Disconnect')
+        newTool = toolbar.AddLabelTool(wx.ID_NEW, 'New', 
+            wx.ArtProvider.GetBitmap(wx.ART_NEW), wx.NullBitmap, 
+            wx.ITEM_NORMAL, 'New Connection')
+        ReScanAPs = toolbar.AddLabelTool(wx.ID_ANY, 'Scan', 
+            wx.Bitmap('imgs/APScan.png'), wx.NullBitmap, 
+            wx.ITEM_NORMAL, 'Scan')
+        connectSe = toolbar.AddLabelTool(wx.ID_ANY, 'Connect', 
+            wx.Bitmap('imgs/connect.png'), wx.NullBitmap, 
+            wx.ITEM_NORMAL, 'Connect')
+        dConnectSe = toolbar.AddLabelTool(wx.ID_ANY, 'Disconnect', 
+            wx.Bitmap('imgs/disconnect.png'), wx.NullBitmap, 
+            wx.ITEM_NORMAL, 'Disconnect')
         toolbar.AddSeparator()
-        quitTool = toolbar.AddLabelTool(wx.ID_EXIT, 'Quit', wx.ArtProvider.GetBitmap(wx.ART_QUIT), wx.NullBitmap, wx.ITEM_NORMAL, 'Quit')
+        quitTool = toolbar.AddLabelTool(wx.ID_EXIT, 'Quit', 
+            wx.ArtProvider.GetBitmap(wx.ART_QUIT), wx.NullBitmap, 
+            wx.ITEM_NORMAL, 'Quit')
         toolbar.Realize()
         # End Toolbar #
 
@@ -134,9 +153,8 @@ class WiFiz(wx.Frame):
             str(self.UIDValue).strip()
             f.close()
         else:
-            self.UID = wx.TextEntryDialog(self, "What is your Interface Name? (wlan0, wlp2s0)",
-                                          "Wireless Interface",
-                                          "")
+            self.UID = wx.TextEntryDialog(self, "What is your Interface Name? "
+                "(wlan0, wlp2s0)", "Wireless Interface", "")
             if self.UID.ShowModal() == wx.ID_OK:
                 self.UIDValue = self.UID.GetValue()
                 f = open(intFile, 'w')
@@ -176,10 +194,12 @@ class WiFiz(wx.Frame):
             os.system("ip link set down dev " + self.UIDValue)
             os.system("netctl start wifiz-" + str(nameofProfile).strip())
             self.OnScan(self)
-            wx.MessageBox("You are now connected to " + str(nameofProfile).strip() + ".", "Connected.")
+            wx.MessageBox("You are now connected to " + 
+                str(nameofProfile).strip() + ".", "Connected.")
         else:
             f = open(workDir + filename, "w")
-            f.write("Description='A profile made by Wifiz for " + str(nameofProfile).strip() + "'\n")
+            f.write("Description='A profile made by Wifiz for " + 
+                str(nameofProfile).strip() + "'\n")
             f.close()
             f = open(workDir + filename, 'a')
             f.write("Interface=" + str(self.UIDValue).strip() + "\n")
@@ -197,9 +217,9 @@ class WiFiz(wx.Frame):
                 f.write(r'Key=\"' + thepass + "\n")
             else:
                 f.write(r'Key=None\n')
-            pref = wx.TextEntryDialog(self, "Is this network preferred? (In range of multiple profiles, connect to this first?)"
-                                      "Preferred Network?",
-                                      "")
+            pref = wx.TextEntryDialog(self, "Is this network preferred? "
+                "(In range of multiple profiles, connect to this first?)"
+                                                "Preferred Network?", "")
             if pref.ShowModal() == wx.ID_OK:
                 f.write("#preferred=yes\n")
             else:
@@ -209,7 +229,8 @@ class WiFiz(wx.Frame):
             os.system("ip link set down dev " + self.UIDValue)
             os.system("netctl enable wifiz-" + str(nameofProfile).strip())
             os.system("netctl start wifiz-" + str(nameofProfile).strip())
-            wx.MessageBox("You are now connected to " + str(nameofProfile).strip() + ".", "Connected.")
+            wx.MessageBox("You are now connected to " + 
+                str(nameofProfile).strip() + ".", "Connected.")
             self.OnScan(self)
 
     def getSelectedIndices( self, state =  wx.LIST_STATE_SELECTED):
@@ -229,7 +250,8 @@ class WiFiz(wx.Frame):
         return indices  
 
     def OnReport(self, e):
-        wx.MessageBox("To report a message, send an email to cody@seafiresoftware.org", "e-Mail")
+        wx.MessageBox("To report a message, send an email to "
+                            "cody@seafiresoftware.org", "e-Mail")
 
     def OnShowPopup(self, e):
         x, y = e.GetPosition()
@@ -244,7 +266,8 @@ class WiFiz(wx.Frame):
         os.system("netctl stop wifiz-" + nameofProfile)
         os.system("ip link set down dev " + self.UIDValue)
         self.OnScan(self)
-        wx.MessageBox("You are now disconnected from " + nameofProfile + ".", "Disconnected.")
+        wx.MessageBox("You are now disconnected from " + 
+                    nameofProfile + ".", "Disconnected.")
 
     def OnNew(self, e):
         newProf = NewProfile(parent=None)       
@@ -258,7 +281,8 @@ class WiFiz(wx.Frame):
             pass
             #why is this here again?
 
-        output = str(subprocess.check_output("iwlist " + self.UIDValue + " scan", shell=True))
+        output = str(subprocess.check_output("iwlist " + self.UIDValue + 
+                                                    " scan", shell=True))
         f = open(logfile, 'w')
         f.write(output)
         f.close()
@@ -266,7 +290,8 @@ class WiFiz(wx.Frame):
         self.APList.DeleteAllItems()
         self.index = 0 
         ilogfile = os.getcwd() + "/iwconfig.log"
-        outputs = str(subprocess.check_output("iwconfig " + self.UIDValue , shell=True))  
+        outputs = str(subprocess.check_output("iwconfig " + 
+                                self.UIDValue , shell=True))  
         d = open(ilogfile, 'w')
         d.write(outputs)
         d.close()
@@ -292,7 +317,10 @@ class WiFiz(wx.Frame):
                 self.index + 1                
                 s = str(line)
                 s2 = s[28:33]
-                s3 = str(int(round(float(s2[0]) / float(s2[3]) * 100))).rjust(3) + " %" # Courtesy of gohu's iwlistparse.py, slightly modified. https://bbs.archlinux.org/viewtopic.php?id=88967
+                # Courtesy of gohu's iwlistparse.py, slightly modified. 
+                # https://bbs.archlinux.org/viewtopic.php?id=88967
+                s3 = str(int(round(float(s2[0]) / float(s2[3]) 
+                * 100))).rjust(3) + " %" 
                 self.APList.SetStringItem(self.index, 1, s3)
             if "Encryption" in line:
                 if "WPA2" in line:
@@ -322,6 +350,7 @@ class WiFiz(wx.Frame):
         app.Exit()      
 
     def OnAbout(self, e):
+        # TODO move this to a file! It doesn't belong 'in code'
         description = """WiFiz is a simple to use, elegant, and powerful frontend for NetCTL. NetCTL is a fork of NetCFG that focuses on being very well integrated into systemd."""
 
         licence = """Copyright (c) 2013 Cody Dostal, Pivotraze
@@ -356,39 +385,46 @@ class NewProfile(wx.Dialog):
     def InitUI(self):
         wizard = wx.wizard.Wizard(None, -1, "New Profile Wizard")
         page1 = TitledPage(wizard, "Interface")
-        page1.Sizer.Add(wx.StaticText(page1, -1, "Please type the name of the interface you will be \nusing to connect to the network. Examples are \nwlan0, eth0, etc..."))
+        page1.Sizer.Add(wx.StaticText(page1, -1, "Please type the name of the "
+            "interface you will be \nusing to connect to the network. "
+            "Examples are \nwlan0, eth0, etc..."))
         page1.Sizer.Add(wx.StaticText(page1, -1, ""))
         intFaces = os.listdir("/sys/class/net")
         for i in intFaces:
             page1.Sizer.Add(wx.RadioButton(page1, -1, i))
         page2 = TitledPage(wizard, "Connection Type")
-        page2.Sizer.Add(wx.StaticText(page2, -1, "Please type the type of connection you will be using."))
+        page2.Sizer.Add(wx.StaticText(page2, -1, 
+            "Please type the type of connection you will be using."))
         page2.Sizer.Add(wx.StaticText(page2, -1, ""))
         choiceWiFi = wx.RadioButton(page2, -1, "Wireless", style=wx.RB_GROUP)
         choiceEth = wx.RadioButton(page2, -1, "Ethernet")
         page2.Sizer.Add(choiceWiFi)
         page2.Sizer.Add(choiceEth)
         page3 = TitledPage(wizard, "Security Type")
-        page3.Sizer.Add(wx.StaticText(page3, -1, "Please type the security type of the connection."))
+        page3.Sizer.Add(wx.StaticText(page3, -1, 
+            "Please type the security type of the connection."))
         page3.Sizer.Add(wx.StaticText(page3, -1, ""))
         choiceWEP = wx.RadioButton(page3, -1, "WEP", style=wx.RB_GROUP)
         choiceWPA = wx.RadioButton(page3, -1, "WPA/WPA2")
         page3.Sizer.Add(choiceWEP)
         page3.Sizer.Add(choiceWPA)
         page4 = TitledPage(wizard, "SSID")
-        page4.Sizer.Add(wx.StaticText(page4, -1, "Please type the name of the network you wish to connect to."))
+        page4.Sizer.Add(wx.StaticText(page4, -1, 
+            "Please type the name of the network you wish to connect to."))
         page4.Sizer.Add(wx.StaticText(page4, -1, ""))
         SSIDV = wx.TextCtrl(page4)
         page4.Sizer.Add(SSIDV, flag=wx.EXPAND)  
         SSID = SSIDV.GetValue()
         page5 = TitledPage(wizard, "Security Key")
-        page5.Sizer.Add(wx.StaticText(page5, -1, "Please type the password of the network you wish to connect to."))
+        page5.Sizer.Add(wx.StaticText(page5, -1, 
+            "Please type the password of the network you wish to connect to."))
         page5.Sizer.Add(wx.StaticText(page5, -1, ""))
         self.securePass = wx.TextCtrl(page5, style=wx.TE_PASSWORD)
         page5.Sizer.Add(self.securePass, flag=wx.EXPAND)
         self.password = self.securePass.GetValue()
         page6 = TitledPage(wizard, "Hidden Network")
-        page6.Sizer.Add(wx.StaticText(page6, -1, "Is the network hidden? If so, check the checkbox."))
+        page6.Sizer.Add(wx.StaticText(page6, -1, 
+            "Is the network hidden? If so, check the checkbox."))
         page6.Sizer.Add(wx.StaticText(page6, -1, ""))
         page6.Sizer.Add(wx.CheckBox(page6, -1, "Network is Hidden"))
 
@@ -409,7 +445,8 @@ class NewProfile(wx.Dialog):
         wx.MessageBox(self.connectionType, self.connectionType, wx.ID_OK)
 
     def closeDialog(self, e):
-        dial = wx.MessageDialog(None, "Are you sure you want to cancel?", "Cancel?", wx.YES_NO|wx.NO_DEFAULT|wx.ICON_QUESTION)
+        dial = wx.MessageDialog(None, "Are you sure you want to cancel?", 
+            "Cancel?", wx.YES_NO|wx.NO_DEFAULT|wx.ICON_QUESTION)
         ret = dial.ShowModal()
         if ref == wx.ID_YES:
             self.Destroy()
