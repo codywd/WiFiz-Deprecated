7fd14f61e9c7372cc0c2495e242a7c3c22287772
Gregory Mullen <greg@grayhatter.com>
2013-04-30 21:08:43 -0700

    Provide package file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2
+
+from distutils.core import setup
+
+setup(name='wifiz',
+      version='0.9.0',
+      description = "GUI for netctl",
+      author = "Cody Dostal, Gregory Mullen",
+      author_email = "greg@grayhatter.com",
+      url = "https://github.com/GrayHatter/WiFiz",
+      py_modules=['foo'],
+      #'runner' is in the root.
+      scripts = ['scripts/wifiz'],
+      long_description = read('README.md')
+      )
