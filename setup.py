b49205f70ffe109e2907a2644877a84d926c1790
Gregory Mullen <greg@grayhatter.com>
2013-05-01 11:46:08 -0700

    Working?

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ setup(name='wifiz',
       author = "Cody Dostal, Gregory Mullen",
       author_email = "greg@grayhatter.com",
       url = "https://github.com/GrayHatter/WiFiz",
-      py_modules=['main'],
+      #py_modules=['main'],
       #'runner' is in the root.
       scripts = ['scripts/wifiz']
+      data_files=[('/usr/share/wifiz', 'main.py'),
+                  ('/usr/share/wifiz/imgs/', 'imgs/*.png')
+                 ]
       )
